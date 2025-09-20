78b42e5a93e0bd62085473369fb91a52b50c62fc
Nguyễn Thanh Nam <thanhnam14112005@gmail.com>
2025-09-20 14:09:19 +0700

    nemn

--- Program.cs
+++ Program.cs
@@ -1,23 +1,2 @@
-using Microsoft.EntityFrameworkCore;
-using LearnApiNetCore.Entity;
-using Microsoft.EntityFrameworkCore.SqlServer;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add DbContext with SQL Server
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddControllers();
-
-var app = builder.Build();
-
-if(app.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI();
-}
-
-app.MapControllers();
-
-app.Run();
+nemnchua
 
