8752aae51a3034ad86329f4f03508e22a975e6aa
Vo Le Hoang Nha <hoangnha10082005@gmail.com>
2025-09-20 14:21:12 +0700

    nha

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -4,7 +4,7 @@ namespace Name
   {
     public string SayHello(string name)
     {
-      return $"Hello, {name}!";
+      return $"xin chao moi nguoi, {name}!";
     }
 
     public string SayGoodbye(string name)
