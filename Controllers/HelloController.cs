1aca2b51edbff1a40ae74133c135ced947f87814
Nguyễn Quốc Trọng <quoctrongnguyen3479@gmail.com>
2025-09-20 14:09:35 +0700

    testing

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -2,9 +2,19 @@ namespace Name
 {
   public class HelloController
   {
-    public string SayHello(string name)
+    public string SayHello(string quanDown)
     {
-      return $"Hello, {name}!";
+      return $"Hello, {quanDown}!";
+    }
+
+    public string Eat(string quanDown)
+    {
+        return $"{quanDown} is eating";
+    }
+
+    public string DiDaiDiIa(string quanDown)
+    {
+        return $"{quanDown} dang di skibidi";
     }
   }
 }
