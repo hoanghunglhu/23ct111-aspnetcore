0e47f89c618a7190f0d8699d6f01dbf178fef765
Hoàng Sỹ Hùng <hoanghunglhu@gmail.com>
2025-09-20 15:34:07 +0700

    fixbug run

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -1,29 +1,20 @@
 namespace Name
 {
-    public class HelloController
-    {
-<<<<<<< HEAD
+  public class HelloController
+  {
     public string SayHello(string name)
-        {
+    {
       return $"Hello, {name}!";
-        }
     }
-<<<<<<< HEAD
-}
-=======
 
     public string SayGoodbye(string name)
     {
       return $"Goodbye, {name}!";
     }
-    
+
     public string goodbyebye(string name)
     {
       return name;
-=======
-      return $"mai tung, {name}!";
->>>>>>> 56c3d08ada1ce45117ee97b00beac262d17d9bcf
     }
   }
 }
->>>>>>> e1c08d98e0a5a312d0ad01c8ecc7880cc49bbc63
