acb0e88613a6e0495c8a9fac488f614f3f517170
Tống Anh Kha <khatong072@gmail.com>
2025-09-20 14:07:38 +0700

    upload

--- Entity/AppDbContext.cs
+++ Entity/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace LearnApiNetCore.Entity;
 public class AppDbContext : DbContext
 {
   public AppDbContext(DbContextOptions<AppDbContext> options)
-  : base(options) { }
+
 
   public DbSet<User> Users { get; set; }
 }
