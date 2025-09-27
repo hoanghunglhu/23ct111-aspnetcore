fee82b4b79883e5e6c4a4dd118943f01eb435eb1
Lê Hoàng Tuấn <lehoangtuan0973@gmail.com>
2025-09-27 15:46:07 +0700

    .

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
-+
+
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
