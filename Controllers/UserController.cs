8dbe459e61875a60b7adb337206e8abde59e9563
Nguyễn Đỉnh <dinhnguyen.27062005@gmail.com>
2025-09-27 15:56:35 +0700

    .

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -21,11 +21,25 @@ namespace LearnApiNetCore.Controllers
     [HttpGet]
     public IActionResult GetAll()
     {
-      var users = new List<Models.UserModel>();
+      var users = _context.Users.ToList();
+      return Ok(users);
+    }
 
-      users.Add(new Models.UserModel { Id = 1, Name = "Nguyen Van A", Email = "" });
-      users.Add(new Models.UserModel { Id = 2, Name = "Nguyen Van B", Email = "" });
-      users.Add(new Models.UserModel { Id = 3, Name = "Nguyen Van C", Email = "" });
+    [HttpPost]
+    public IActionResult Create(UserModel model)
+    {
+      var user = new User
+      {
+        name = model.name,
+        email = model.email,
+        phone = model.phone,
+        address = model.address,
+        birthday = model.birthday,
+        gender = model.gender
+      };
+
+      _context.Users.Add(user);
+      _context.SaveChanges();
 
       return CreatedAtAction(nameof(GetById), new { id = user.id }, user);
     }
@@ -33,7 +47,45 @@ namespace LearnApiNetCore.Controllers
     [HttpGet("{id}")]
     public IActionResult GetById(int id)
     {
-      return new UserModel { Id = 1, Name = "Nguyen Van A", Email = "" };
+      var user = _context.Users.Find(id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+      return Ok(user);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, UserModel model)
+    {
+      var user = _context.Users.Find(id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      user.name = model.name;
+      user.email = model.email;
+      user.phone = model.phone;
+      user.address = model.address;
+      user.birthday = model.birthday;
+      user.gender = model.gender;
+      _context.SaveChanges();
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+      var user = _context.Users.Find(id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      _context.Users.Remove(user);
+      _context.SaveChanges();
+      return NoContent();
     }
   }
 }
