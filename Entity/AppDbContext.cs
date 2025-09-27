65c0c7c35c620ba7e9a4f9e429d8ce586af7660f
Tống Anh Kha <khatong072@gmail.com>
2025-09-27 15:28:20 +0700

    fasdf

--- Entity/AppDbContext.cs
+++ Entity/AppDbContext.cs
@@ -4,7 +4,7 @@ namespace LearnApiNetCore.Entity;
 
 public class AppDbContext : DbContext
 {
-  public AppDbContext(DbContextOptions<AppDbContext> options)
+  public AppDbContext(DbContextOptions<AppDbContext> options):base (options){}
 
 
   public DbSet<User> Users { get; set; }
