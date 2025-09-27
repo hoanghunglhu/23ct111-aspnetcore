313773078b03c0079bfc0943b5668e6b3d5d3537
Lê Hoàng Tuấn <lehoangtuan0973@gmail.com>
2025-09-27 14:03:03 +0700

    tuine

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -4,7 +4,7 @@ namespace Name
   {
     public string SayHello(string name)
     {
-      return $"kkk, {name}!";
+      return $"lehoangtuan, {name}!";
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -8,14 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
++
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
-if(app.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI();
-}
 
 app.MapControllers();
 
