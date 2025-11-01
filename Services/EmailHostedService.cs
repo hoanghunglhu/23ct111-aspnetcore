101cc3e40d90110335160a4dc8576c0451224c05
Nguyễn Thanh Nam <thanhnam14112005@gmail.com>
2025-11-01 16:19:43 +0700

    1-11-2025-bt

--- Services/EmailHostedService.cs
+++ Services/EmailHostedService.cs
@@ -1,24 +1,16 @@
-using Microsoft.Extensions.Hosting;
 using System;
-using System.Net.Mail;
 using System.Net;
+using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.Hosting;
 
 namespace LearnApiNetCore.Services
 {
     public class EmailHostedService : IHostedService, IDisposable
     {
         private Timer _timer;
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _timer = new Timer(SendEmail, null, TimeSpan.Zero, TimeSpan.FromMinutes(10));
-            return Task.CompletedTask;
-        }
-
-        private void SendEmail(object state)
+        private void SendEmail()
         {
             string fromAddress = "thanhnam14112005@gmail.com";
             string toAddress = "bemo14112005@gmail.com";
@@ -37,29 +29,42 @@ namespace LearnApiNetCore.Services
     </div>
   </body>
 </html>";
-
-
-            using(var smtp = new SmtpClient("smtp.gmail.com", 587))
+            using (var smtp = new SmtpClient("smtp.gmail.com", 587))
             {
                 smtp.EnableSsl = true;
                 smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
                 smtp.UseDefaultCredentials = false;
                 smtp.Credentials = new NetworkCredential(fromAddress, password);
 
-                using(var message = new MailMessage(fromAddress, toAddress, subject, body))
+                using (var message = new MailMessage(fromAddress, toAddress, subject, body))
                 {
                     message.IsBodyHtml = true;
                     smtp.Send(message);
                 }
             }
         }
-
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
+            return Task.CompletedTask;
+        }
+        private void DoWork(object state)
+        {
+            try
+            {
+                SendEmail();
+                Console.WriteLine($"[{DateTime.Now}] Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[{DateTime.Now}] Error sending email: {ex.Message}");
+            }
+        }
         public Task StopAsync(CancellationToken cancellationToken)
         {
             _timer?.Change(Timeout.Infinite, 0);
             return Task.CompletedTask;
         }
-
         public void Dispose()
         {
             _timer?.Dispose();
