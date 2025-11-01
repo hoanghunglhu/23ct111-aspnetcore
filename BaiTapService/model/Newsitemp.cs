1284964ba035a0a4f980be3be974678cf14a8a1e
Nguyễn Đỉnh <dinhnguyen.27062005@gmail.com>
2025-11-01 15:15:11 +0700

    .

---
+++ BaiTapService/model/Newsitemp.cs
@@ -0,0 +1,9 @@
+namespace YourProjectName.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
